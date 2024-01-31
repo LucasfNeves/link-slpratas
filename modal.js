344916c1eac48fe4dbbece1771f4159c1e1fc479
LucasfNeves <lucasfarias.fn@gmail.com>
2024-01-30 22:24:52 -0300

    edited modal.js

--- modal.js
+++ modal.js
@@ -1,15 +1,18 @@
-const modal = document.querySelector('.modal');
-const btnClose = document.querySelector('.btn-close');
-const btnOpen = document.querySelector('.btn-open');
+(function(){
+    const modal = document.querySelector('.modal');
+    const btnClose = document.querySelector('.btn-close');
+    const btnOpen = document.querySelector('.btn-open');
 
-function openModal() {
-    modal.style.display = 'flex';
-}
+    function openModal() {
+        modal.style.display = 'flex';
+    }
 
-function closeModal() {
-    modal.style.display = 'none';
-}
+    function closeModal() {
+        modal.style.display = 'none';
+    }
 
-btnOpen.addEventListener('click', openModal);
+    btnOpen.addEventListener('click', openModal);
 
-btnClose.addEventListener('click', closeModal);
+    btnClose.addEventListener('click', closeModal);
+
+})(); // IIFE - Immediately Invoked Function Expression
