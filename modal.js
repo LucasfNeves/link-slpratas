f9506ff519918ce56594a75ccdaf8751ba6f5281
LucasfNeves <lucasfarias.fn@gmail.com>
2024-01-30 22:16:48 -0300

    modal

---
+++ modal.js
@@ -0,0 +1,15 @@
+const modal = document.querySelector('.modal');
+const btnClose = document.querySelector('.btn-close');
+const btnOpen = document.querySelector('.btn-open');
+
+function openModal() {
+    modal.style.display = 'flex';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+btnOpen.addEventListener('click', openModal);
+
+btnClose.addEventListener('click', closeModal);
